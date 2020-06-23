c01678aee730c2ba182edcda64bb5193d186a060
Soohwan Kim <kimsoohwan@sunmoon.ac.kr>
2020-06-23 16:05:09 +0900

    Create the messages and winning rule

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,6 +3,13 @@ from random import randint
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
 
+# list of messages
+messages = ["Tie!", "You win!", "You lose!"]
+
+# game rule
+winning_rule = {"Rock": "Scissors", "Paper": "Rock", 
+                "Scissors": "Paper"}
+
 # get the user input
 player = input("Rock, Paper, Scissors? ")
 
